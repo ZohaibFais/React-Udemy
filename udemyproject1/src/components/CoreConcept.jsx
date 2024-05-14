0a88731fa48a6a91e207e6cea9041db2b4464556
ZohaibFais <murtuza.tcc@gmail.com>
2024-05-14 14:20:11 +0500

    project1

--- udemyproject1/src/components/CoreConcept.jsx
+++ udemyproject1/src/components/CoreConcept.jsx
@@ -1,25 +1,9 @@
-
-// function CoreConcept(props){    //Method1 of calling props
-//       return (
-//       <li>
-//         <img src={props.image} alt={props.title} />
-//         <h3>{props.title}</h3>
-//         <p>{props.description}</p>
-//       </li> 
-//       )
-//     }
-    
-    
-
-export default function CoreConcept({ image, title, description }) {  //shorter method
-
-    return (
-      <li>
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    );
-  
-  }
-  
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
